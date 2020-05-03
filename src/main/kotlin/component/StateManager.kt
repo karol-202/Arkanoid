807829b752da224793129008724ea138f2e72138
karol-202 <karoljurski1@gmail.com>
2020-05-03 12:39:10 +0200

    Fix state publisher clearing

--- src/main/kotlin/component/StateManager.kt
+++ src/main/kotlin/component/StateManager.kt
@@ -9,22 +9,37 @@ import update.UpdateContext
 
 data class StateManager<S : State>(val state: S) : Component
 {
+	private data class TraversalResult<S : State>(val gameObject: GameObject,
+	                                              val stateChanges: List<StateChange<S>> = emptyList())
+	{
+		fun traverse(): TraversalResult<S> = traverseSelf().traverseChildren()
+
+		private fun traverseSelf(): TraversalResult<S>
+		{
+			val result = gameObject.getComponent<StatePublisher<S>>()?.getChangesWithClearedPublisher() ?: return this
+			val newObject = gameObject.withComponentReplaced(result.publisher)
+			return withGameObject(newObject).withAddedChanges(result.stateChanges)
+		}
+
+		private fun traverseChildren() =
+				gameObject.children.fold(this) { currentState, child ->
+					val childResult = TraversalResult<S>(child).traverse()
+					val newChild = currentState.gameObject.withChildReplaced(child, childResult.gameObject)
+					currentState.withGameObject(newChild).withAddedChanges(childResult.stateChanges)
+				}
+
+		fun withGameObject(gameObject: GameObject) = copy(gameObject = gameObject)
+
+		fun withAddedChanges(newChanges: List<StateChange<S>>) = copy(stateChanges = stateChanges + newChanges)
+	}
+
 	override fun update(ownerNode: ParentNode.GameObjectNode, updateContext: UpdateContext) = update(ownerNode) {
-		owner.withComponentReplaced(withUpdatedState(updatedState(owner)))
+		val traversalResult = TraversalResult<S>(owner).traverse()
+		val newState = traversalResult.stateChanges.applyTo(state)
+		traversalResult.gameObject.withComponentReplaced(withUpdatedState(newState))
 	}
 
-	private fun updatedState(owner: GameObject) = findStateChanges(owner).fold(state) { currentState, change ->
-		currentState.change()
-	}
-
-	private fun findStateChanges(owner: GameObject) =
-			findPublishers(owner).flatMap { it.getChangesWithClearedPublisher().stateChanges /* TODO Clear publisher */ }
-
-	private fun findPublishers(gameObject: GameObject): List<StatePublisher<S>> =
-			listOfNotNull(gameObject.getComponent<StatePublisher<S>>()) + findPublishersInChildren(gameObject)
-
-	private fun findPublishersInChildren(gameObject: GameObject) =
-			gameObject.children.flatMap { findPublishers(it) }
+	private fun List<StateChange<S>>.applyTo(state: S) = fold(state) { currentState, change -> currentState.change() }
 
 	private fun withUpdatedState(state: S) = copy(state = state)
 }
